03e4bfac157d4b14a0cf6d1206101e2b8264c916
gustavojoze <gustavojosesoaressantana@gmail.com>
2025-09-13 23:22:39 -0300

    atualizando até o 9° teste

--- fatura-back1/src/test/java/com/fatec/fatura/persistencia/TUReq16EmissaoDaFaturaTests.java
+++ fatura-back1/src/test/java/com/fatec/fatura/persistencia/TUReq16EmissaoDaFaturaTests.java
@@ -35,6 +35,7 @@ class TUReq16EmissaoDaFaturaTests {
 		}
 
 	}
+
 	@Test
 	void ct02_quando_dados_invalidos_retorna_msg_erro() {
 		try {
@@ -48,6 +49,126 @@ class TUReq16EmissaoDaFaturaTests {
 			logger.info(">>>>>> ct01 - nao deveria falhar => " + e.getMessage());
 			assertEquals("CPF invalido", e.getMessage());
         }
+
+	}
+	
+	@Test
+	void ct03_quando_dados_invalidos_cpf_branco_msg_de_erro() {
+		try {
+			// dado que as informacoes de fatura sao validas
+			// quando confirmo a fatura
+			LocalDate dataVencimento = LocalDate.parse("02/10/2025", formatter);
+			fatura = new Fatura(" ", dataVencimento, "moveis planejados", "1000.50");
+			// entao fatura é registrada com data de emisssao igual a data de hoje
+
+		} catch (Exception e) {
+			logger.info(">>>>>> ct01 - nao deveria falhar => " + e.getMessage());
+			assertEquals("CPF invalido", e.getMessage());
+
+		}
+
+	}
+
+	@Test
+	void ct04_quando_dados_invalidos_cpf_null_msg_de_erro() {
+		try {
+			// dado que as informacoes de fatura sao invalidas
+			// quando confirmo a fatura
+			LocalDate dataVencimento = LocalDate.parse("02/10/2026", formatter);
+			fatura = new Fatura(null, dataVencimento, "moveis planejados", "1000.50");
+			// entao fatura é registrada com data de emisssao igual a data de hoje
+
+		} catch (Exception e) {
+			logger.info(">>>>>> ct01 - nao deveria falhar => " + e.getMessage());
+			assertEquals("CPF invalido", e.getMessage());
+
+		}
+
+	}
+
+	@Test
+	void ct05_quando_dados_invalidos_cpf_formatado_msg_de_erro() {
+		try {
+			// dado que as informacoes de fatura sao invalidas
+			// quando confirmo a fatura
+			LocalDate dataVencimento = LocalDate.parse("02/10/2026", formatter);
+			fatura = new Fatura("390.863.600-09", dataVencimento, "moveis planejados", "1000.50");
+			// entao fatura é registrada com data de emisssao igual a data de hoje
+
+		} catch (Exception e) {
+			logger.info(">>>>>> ct01 - nao deveria falhar => " + e.getMessage());
+			assertEquals("CPF invalido", e.getMessage());
+
+		}
+
+	}
+
+	@Test
+	void ct06_quando_dados_invalidos_cpf_caractere_iguais_msg_de_erro() {
+		try {
+			// dado que as informacoes de fatura sao invalidas
+			// quando confirmo a fatura
+			LocalDate dataVencimento = LocalDate.parse("02/10/2026", formatter);
+			fatura = new Fatura("111.111.111-11", dataVencimento, "moveis planejados", "1000.50");
+			// entao fatura é registrada com data de emisssao igual a data de hoje
+
+		} catch (Exception e) {
+			logger.info(">>>>>> ct01 - nao deveria falhar => " + e.getMessage());
+			assertEquals("CPF invalido", e.getMessage());
+
+		}
+
+	}
+
+	@Test
+	void ct07_quando_dados_invalidos_cpf_12_caracteres_msg_de_erro() {
+		try {
+			// dado que as informacoes de fatura sao invalidas
+			// quando confirmo a fatura
+			LocalDate dataVencimento = LocalDate.parse("02/10/2026", formatter);
+			fatura = new Fatura("111.111.111-112", dataVencimento, "moveis planejados", "1000.50");
+			// entao fatura é registrada com data de emisssao igual a data de hoje
+
+		} catch (Exception e) {
+			logger.info(">>>>>> ct01 - nao deveria falhar => " + e.getMessage());
+			assertEquals("CPF invalido", e.getMessage());
+
+		}
+
+	}
+
+	@Test
+	void ct08_quando_dados_invalidos_cpf_invalido_msg_de_erro() {
+		try {
+			// dado que as informacoes de fatura sao invalidas
+			// quando confirmo a fatura
+			LocalDate dataVencimento = LocalDate.parse("02/10/2026", formatter);
+			fatura = new Fatura("123.456.789-11", dataVencimento, "moveis planejados", "1000.50");
+			// entao fatura é registrada com data de emisssao igual a data de hoje
+
+		} catch (Exception e) {
+			logger.info(">>>>>> ct01 - nao deveria falhar => " + e.getMessage());
+			assertEquals("CPF invalido", e.getMessage());
+
+		}
+
+	}
+
+	@Test
+	void ct09_quando_data_invalida_msg_de_erro() {
+		try {
+			// dado que as informacoes de fatura sao invalidas
+			// quando confirmo a fatura
+			LocalDate dataVencimento = LocalDate.parse("31/02/2026", formatter);
+			fatura = new Fatura("3a086360009", dataVencimento, "moveis planejados", "1000.50");
+			// entao fatura é registrada com data de emisssao igual a data de hoje
+			logger.info(">>>>>> data de vencimento " + dataVencimento.toString());
+			assertNotNull(fatura);
+		} catch (Exception e) {
+			logger.info(">>>>>> ct09 - nao deveria falhar => " + e.getMessage());
+			fail("nao deveria falhar fatura valida");
+
+		}
 
 	}
 	
